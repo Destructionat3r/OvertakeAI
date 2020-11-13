052100a7691f27b73b7fa05855d2036dff1b753f
Destructionat3r <lukeperry7700@gmail.com>
2020-11-13 10:26:47 +0000

    Add project files.

---
+++ OvertakeAI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OvertakeAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This example fetches and prints the first 1000 data items
+            for (var i = 0; i < 1000; i++)
+            {
+                var overtake = Library.Overtake.GetNextOvertake();
+                //Console.WriteLine($"{overtake.ToString()}\n");
+                Console.WriteLine($"InitialSeparation = {overtake.InitialSeparationM:F1} metres");
+                Console.WriteLine($"OvertakingSpeed = {overtake.OvertakingSpeedMPS:F1} m/s");
+                Console.WriteLine($"OncomingSpeed = {overtake.OncomingSpeedMPS:F1} m/s");
+                Console.WriteLine($"Success = {overtake.Success}\n");
+            }
+
+            Console.ReadKey(); // Keep the window open till a key is pressed
+        }
+    }
+}
